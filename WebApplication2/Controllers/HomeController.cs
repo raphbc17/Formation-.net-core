53fd9327721910a9a081115eff84b23ec849f877
unknown <you@example.com>
2020-02-10 10:54:45 +0100

    Formation .net core

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+using WebApplication2.ViewModels;
+
+namespace WebApplication2.Controlers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
+            var homeViewModel = new HomeViewModel()
+            {
+                Title = "Welcome to Bethany's pie shop",
+                Pies = pies.ToList(),
+            };
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ WebApplication2/Models/MockPieRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class MockPieRepository : IPieRepository
+    {
+        private List<Pie> _pies;
+
+
+        public MockPieRepository()
+        {
+            if (_pies == null)
+            {
+                InitializePies();
+            }
+        }
+
+
+        private void InitializePies()
+        {
+            _pies = new List<Pie>
+            {
+            new Pie { Id=1, Name="Apple Pie", Price=12.95M, ShortDescription="Our famous apple pies!", LongDescription= "aejgcyuagzy"},
+            new Pie { Id = 2, Name = "Blueberry Cheese Cake", Price = 18.95M, ShortDescription = "You'll love it!", LongDescription = "aejgcyuagzy" },
+            new Pie { Id = 3, Name = "Cheese Cake", Price = 18.95M, ShortDescription = "Plain cheese cake. Plain pleasure.", LongDescription = "aejgcyuagzy" },
+            new Pie { Id = 4, Name = "Cherry Pie", Price = 14.95M, ShortDescription = "A summer classical!", LongDescription = "aejgcyuagzy" },
+            };
+        }
+
+
+
+        public IEnumerable<Pie> GetAllPies()
+        {
+            return _pies;
+        }
+        public Pie GetPieById(int pieId)
+        {
+            return _pies.FirstOrDefault(p => p.Id == pieId);
+        }
+    }
+}
